c6ae4dfd8d67f2f2d02f7ae2c7f9ccdf883002bb
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-05-03 09:21:26 -0400

    Organize files and format code

--- Source/ViewController.h
+++ Source/ViewController.h
@@ -9,7 +9,4 @@
 @import UIKit;
 
 @interface ViewController : UIViewController
-
-
 @end
-
---
+++ Tests/UnitTests/DynamicColor2Tests.m
@@ -0,0 +1,40 @@
+//
+//  DynamicColor2Tests.m
+//  DynamicColor2Tests
+//
+//  Created by Jeffrey Kereakoglow on 2/17/15.
+//  Copyright (c) 2015 Alexis Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface DynamicColor2Tests : XCTestCase
+
+@end
+
+@implementation DynamicColor2Tests
+
+- (void)setUp {
+  [super setUp];
+  // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+  // Put teardown code here. This method is called after the invocation of each test method in the class.
+  [super tearDown];
+}
+
+- (void)testExample {
+  // This is an example of a functional test case.
+  XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+  // This is an example of a performance test case.
+  [self measureBlock:^{
+    // Put the code you want to measure the time of here.
+  }];
+}
+
+@end
